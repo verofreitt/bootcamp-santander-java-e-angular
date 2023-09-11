ed0f21ab3515cb9b0f69085ead569051504c0c0c
Verônica Freitas Santos <verofreit@gmail.com>
2023-09-11 20:24:12 -0300

    proj blog

---
+++ CURSO ANGULAR/PROJETOS ANGULAR/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,50 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Shibuya vira campo de batalha entre feiticeiros e maldições",
+    "description":"Após muitas lutas e imensas baixas Satoru Gojo foi selado e hospedeiro do Sukuna setenciado a morte, qualquer um que tentar ajuda-los será considerado cúmplice.",
+    "photoCover": "https://www.dexerto.com/cdn-cgi/image/width=3840,quality=75,format=auto/https://editors.dexerto.com/wp-content/uploads/2023/06/15/jujutsu-kaisen-gojo-vs-sukuna.jpeg"
+  },
+  {
+    "id":"2",
+    "title":"Luffy revela que o One Piece são os amigos que fazemos no caminho",
+    "description":"Após anos viajando em alto mar, vivendo incriveis aventuras Luffy relata que seu sonho sempre foi se tornar o Rei dos Piratas, mas que com o tempo que passou com seu acompanheiros e os amigos que fez durante sua jornada, ele afirma que se não encontrasse o One Piece a viagem já teria valido a pena",
+    "photoCover": "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/1ecde018e863e2aaee31f00a23378c35.jpe"
+  },
+  {
+    "id":"3",
+    "title":"Scott Pilgrim admite que adora bater em ex malvado",
+    "description":"Após ser pressionado, Scott revela que era satisfatório lutar com os ex's namorados de Ramona, pois assim ele poderia se vingar de outros já terem estado com ela antes dele",
+    "photoCover":"https://gamerview.uai.com.br/wp-content/uploads/2021/01/scott-pilgrim-vs-the-world-bg.jpg"
+  },
+  {
+    "id":"4",
+    "title":"Pokémons fazem abaixo assinado por pokebolas maiores e mais confortáveis",
+    "description":"Depois de muitas lutas e viagens, pokemons só gostariam de um lugar maior e mais aconchegante para descançar, eles sempre lutam contra outros pokemons sem pedir nada em troca aos treinadores, revoltados com a situação fizeram um abaixo assinado por condições melhores e um pouco mais de consideração por parte daqueles que tanto usam seus trabalhos",
+    "photoCover":"https://www.gamespot.com/a/uploads/screen_kubrick/1601/16018044/4056259-gen-one-pokemon.jpg"
+  },
+  {
+    "id":"5",
+    "title":"Ichigo revela se ficou dividido entre Rukia e Orihime",
+    "description":"Ichigo abre o coração e confessa que por alguns momentos se sentiu atraido por Rukia, porém o laço de amizade dos dois foi crescendo exponencialmente e o que poderia virar um grande amor, acabou por se torna uma sincera amizade",
+    "photoCover":"https://roteironerd.com/wp-content/uploads/2021/08/bleach.jpg.webp"
+  },
+  {
+    "id":"6",
+    "title":"Guts após anos de luta revela que está cansado",
+    "description":"Segundo Guts lutar por tantos anos, ver seu amor passar por tudo que passou e ainda ter seu melhor amigo como pivô de tudo fez com que ele se sentisse cada vez mais exausto, ele só quer viver sua vida em paz ao lado da mulher que ama",
+    "photoCover":"https://criticalhits.com.br/wp-content/uploads/2021/11/berserk.jpg"
+  },
+  {
+    "id":"7",
+    "title":"Kaiman e Nikaido afirman serem só bons amigos",
+    "description":"Mesmo com todos sentindo clima de romance no ar, Kaiman e Nikaido afirman que são apenas bons amigos e dizem que com tantas coisas que passaram juntos esse vinculo de amizade se tornou ainda mais forte",
+    "photoCover":"https://www.syfy.com/sites/syfy/files/2020/07/aaaabesmsf_zi8xgme8roid7sxy_gco6im5-y1bcnrucb0nwfpcf6un4f_l-nljdn8pxokvonbl0ys08kwxmfjeevkh-ahr.jpg"
+  },
+  {
+    "id":"8",
+    "title":"Após anos de hiato Kon e Killua querem voltar a atuar em HxH",
+    "description":"Kon e Killua dizem que não aguentam mais o hiato, atuar em HxH fazia a vida deles mais alegre, após algumas novas paginas do mangá serem lançadas recentemente a velha chama em seus corações reacendeu e fez eles terem esperança que um dia a histaria irá voltar definitivamente",
+    "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2017/05/gon-killua-hunter-hunter-volta-junho-2017.png"
+  },
+]
