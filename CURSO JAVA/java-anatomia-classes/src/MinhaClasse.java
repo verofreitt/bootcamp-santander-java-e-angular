d400ad659d46d1887b1e21758d2fe3ba2138d5ce
Verônica Freitas Santos <verofreit@gmail.com>
2023-09-15 22:17:43 -0300

    introdução ao java

---
+++ CURSO JAVA/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+        //CORPO DA CLASSE
+    public static void main (String [] args) {
+        //CORPO DO METODO MAIN
+        System.out.print ("Hello world");
+        
+    }
+
+}
